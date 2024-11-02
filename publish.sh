0f48704c303a3c4823629ad78d3e0a1640c446cf
Marco Galanti <marco.galanti@gmail.com>
2024-11-02 12:43:13 +0100

    Delete publish.sh

--- publish.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-api-catalog publish-asset --organization 8b67a29a-a22d-4d92-89fe-87f99d911be5 --client_id 2b5330319c8c414986831838e839484a --client_secret A6CE3dfDb0d146c78FD3FadEFe3C6805
